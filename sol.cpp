196e6214a7fd496dc334a80419aac1737b57e7f3
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-06-24 11:29:49 +0800

    Add files via upload

---
+++ sol.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+void decode() {
+	int p1, p2, q1, q2;
+	cin >> p1 >> p2 >> q1 >> q2;
+	int r1 = abs(q1 - p1);
+	int r2 = abs(q2 - p2);
+	cout << r1 + q1 << " " << r2 + q2 << "\n";
+}
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t;
+	cin >> t;
+	while(t--) {
+		decode();
+	}
+	return 0;
+}
